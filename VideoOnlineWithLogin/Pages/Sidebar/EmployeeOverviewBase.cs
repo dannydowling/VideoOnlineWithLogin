d6bc1d7f418924e7d3d7eb111deb8796cf6136a1
Danny Dowling <danny.dowling@gmail.com>
2020-02-23 12:32:12 -0900

    updating pages

---
+++ VideoOnlineWithLogin/Pages/Sidebar/EmployeeOverviewBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using VideoOnlineWithLogin.Server.Pages.Sidebar;
+using VideoOnlineWithLogin.Server.Services;
+using VideoOnlineWithLogin.Shared;
+
+namespace VideoOnlineWithLogin.Server.Pages
+{
+    public class EmployeeOverviewBase: ComponentBase
+    {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        public List<Employee> Employees { get; set; }
+
+        protected AddEmployeeDialog AddEmployeeDialog { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+        }
+
+        public async void AddEmployeeDialog_OnDialogClose()
+        {
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+            StateHasChanged();
+        }
+
+        protected void QuickAddEmployee()
+        {
+            AddEmployeeDialog.Show();
+        }
+    }
+}
