f939055b4d74bce8a4acec0f1549f7a9011c6bb9
Danny Dowling <danny.dowling@gmail.com>
2020-06-03 15:27:13 -0800

    having issues with xamarin working with netcore 3.1

---
+++ PreFlight.Client/PreFlight.Client.iOS/Mapping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace PreFlight.Client.iOS
+{
+    public class Mapping
+    {
+
+        public async Task StartFrom(Coordinates coordinates)
+        {
+            var location = new Location(coordinates);
+            var options = new MapLaunchOptions { Name = "Start Location" };
+
+            await Map.OpenAsync(location, options);
+        }
+
+        public async Task NavigateTo(Coordinates coordinates)
+        {
+            var location = new Location(coordinates);
+            var options = new MapLaunchOptions { NavigationMode = NavigationMode.Direct };
+
+            await Map.OpenAsync(location, options);
+        }
+    }
+}
