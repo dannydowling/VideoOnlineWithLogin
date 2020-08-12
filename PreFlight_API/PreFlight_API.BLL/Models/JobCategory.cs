982b07b1d6b0d36925da67678bca83506ee09bcf
Danny Dowling <danny.dowling@gmail.com>
2020-08-11 19:45:29 -0800

    enum helper for guids ref role enums

--- PreFlight_API/PreFlight_API.BLL/Models/JobCategory.cs
+++ PreFlight_API/PreFlight_API.BLL/Models/JobCategory.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using PreFlight_API.BLL;
 
 namespace PreFlight_API.BLL.Models
 {
     public class JobCategory 
     {
         public Guid Id { get; set; }
-        public string JobCategoryName { get; set; }
+
+        private string _jobCategoryName;
+
+        public string JobCategoryName
+        {
+            // get the name from the enum, whatever's in the backing field, 
+            // of the type, the string associated with the enum...
+
+            get { return EnumHelpers.GetName<JobCategoryEnum>(_jobCategoryName.GetType(), _jobCategoryName); }
+            set { _jobCategoryName = value; }
+        }
     }
     
 
